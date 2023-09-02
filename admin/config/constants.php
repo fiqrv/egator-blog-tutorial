f867da0729b85e6b2b76c962d6435c2d6be0e0ef
fiqrv <fikrifauzi1998@gmail.com>
2023-09-03 00:50:21 +0800

    Changed the ROOT_URL to follow the cloned directory name

--- admin/config/constants.php
+++ admin/config/constants.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-define('ROOT_URL', 'http://localhost/blog/');
+define('ROOT_URL', 'http://localhost/egator-blog-tutorial/');
 define('DB_HOST','localhost');
 define('DB_USER','dbadmin');
 define('DB_PASS','admin123');
